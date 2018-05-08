62aa78917d56a39d84f9a249f78028e4a36e6054
Lord Prashant <lord_prashant@hotmail.com>
2018-05-08 09:44:46 +0200

    Added code

--- Mosh.Intermediate/Constructors/Program.cs
+++ Mosh.Intermediate/Constructors/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//
+
 namespace Constructors
 {
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            :this() // this keyword here initializes the method with no parameters
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            :this(id) // this keyword here initializes the method with the id parameter
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+    public class Order
+    {
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Brohan";
+
+            //var order = new Order(); this is not a good solution. The main method is not responsible for initializing the list
+
+            Console.WriteLine("Name of the person is {0} and the id is {1}", customer.Name, customer.Id);
+            Console.ReadKey();
         }
     }
 }
