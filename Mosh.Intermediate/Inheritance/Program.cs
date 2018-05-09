af0e51b4e73503d30c25849ec7f06f0c49aa3c05
Lord Prashant <lord_prashant@hotmail.com>
2018-05-09 23:24:38 +0200

    New projects added

---
+++ Mosh.Intermediate/Inheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object duplicated");
+        }
+    }
+
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            Console.WriteLine("Added link to " + url);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+
+
+        }
+    }
+}
