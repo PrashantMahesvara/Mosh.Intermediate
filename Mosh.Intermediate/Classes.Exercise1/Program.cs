81987b2a07eb8cde1344cb3cc483932173cfb0ef
Prashant Mahesvara <lord_prashant@hotmail.com>
2018-08-05 14:12:41 +0200

    New commit for sln

---
+++ Mosh.Intermediate/Classes.Exercise1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Classes.Exercise1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hwi! Press Enter to start the clock!");
+
+            Console.ReadKey();
+
+            var stopWatch = new Stopwatch();
+
+            var startTime = Stopwatch.Start(DateTime.Now);
+
+            Console.WriteLine("Time started at: " + startTime);
+
+            Console.ReadKey();
+
+            var stopTime = Stopwatch.Stop(DateTime.Now);
+
+
+            Console.WriteLine("Time stopped at: " + stopTime);
+
+
+            Console.ReadKey();
+
+            
+
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Mosh.Intermediate/Classes.Exercise1/Stopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Exercise1
+{
+    public class Stopwatch
+    {
+        public static DateTime Now { get; set; }
+
+        public static DateTime Start(DateTime time)
+        {
+            var timerStart = time;
+
+            var newTime = timerStart;
+
+            return timerStart;
+        }
+
+        public static DateTime Stop(DateTime time)
+        {
+            var timerStop = DateTime.Now;
+
+            var test = Start(time);
+
+            return timerStop;
+        }
+            
+        public void Duration (DateTime time)
+        {
+            var difference = Start(time) - Stop(time);
+
+            return;
+        }
+    }
+}
